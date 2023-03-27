7a6569e179009314ad5c42eeb001579b43c51aa8
Luis Mateo Michel Flores <101907819+Bussy888@users.noreply.github.com>
2023-03-26 21:37:47 -0400

    Initial commit

---
+++ Assets/Coin/Coin.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * 
+ * NAMESPACE
+ * 
+ */
+
+using System.Collections;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+	public static int coinsCount = 0;
+
+	// Use this for initialization
+	void Start () {
+		Coin.coinsCount++;
+		Debug.Log("Ahora Hay " + Coin.coinsCount + " monedas");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player") == true)
+		{
+			Debug.Log("El personaje colisiono con la moneda");
+			Coin.coinsCount--;
+			if(Coin.coinsCount == 0)
+			{
+				Debug.Log("Juego Completado");
+				GameObject gameManager = GameObject.Find("GameManager");
+				Destroy(gameManager);
+				GameObject[] fireworkSystem = GameObject.FindGameObjectsWithTag("Firework");
+				foreach(GameObject firewors in fireworkSystem)
+				{
+					firewors.GetComponent<ParticleSystem>().Play();
+				}
+			}
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Manager/Timer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour {
+
+	public float maxTime = 60f;
+	private float countdown = 0f;
+
+	// Use this for initialization
+	void Start () {
+		countdown = maxTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		countdown -= Time.deltaTime;
+		Debug.Log("Tiempo: " + countdown);
+		if(countdown <= 0)
+		{
+			Debug.Log("Has perdido!!");
+			Coin.coinsCount = 0;
+			SceneManager.LoadScene("SampleScene");
+		}
+	}
+}
