24f3763ca1a9153a562ac594e9d58e319bc6851f
Luis Mateo Michel Flores <101907819+Bussy888@users.noreply.github.com>
2023-03-29 20:05:48 -0400

    BasicCSharp
    BasicCSharp

---
+++ Assets/Scripts/VariableScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariableScript : MonoBehaviour {
+	public int myNumber = 30;
+	public int total;
+	private int superSecret;
+
+	public int n1 = 5;
+	public int n2 = 0;
+	// Use this for initialization
+	void Start () {
+		total = myNumber - 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Return))
+		{
+			AddTwoNumber();
+		}
+	}
+	void AddTwoNumber() //Void por defecto es privado
+	{
+		Debug.Log(n1 + n2);
+	}
+}
